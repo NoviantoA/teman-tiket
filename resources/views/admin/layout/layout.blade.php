f2c76ca646b8fbf1e0011b45919646988fb263e3
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2023-07-15 10:39:02 +0700

    check

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,44 +21,44 @@
     <!-- endinject -->
     <link rel="shortcut icon" href="{{ url('admin/images/favicon.png') }}" />
 </head>
+
 <body>
-<div class="container-scroller">
-    @include('admin.layout.header')
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
+    <div class="container-scroller">
+        @include('admin.layout.header')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
 
-        @include('admin.layout.sidebar')
+            @include('admin.layout.sidebar')
 
-        @yield('content')
+            @yield('content')
 
-        <!-- main-panel ends -->
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-</div>
-<!-- container-scroller -->
+    <!-- container-scroller -->
 
-<!-- plugins:js -->
-<script src="{{ url('admin/vendors/js/vendor.bundle.base.js') }}"></script>
-<!-- endinject -->
-<!-- Plugin js for this page -->
-<script src="{{ url('admin/vendors/chart.js/Chart.min.js') }}"></script>
-<script src="{{ url('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-<script src="{{ url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-<script src="{{ url('admin/js/dataTables.select.min.js') }}"></script>
+    <!-- plugins:js -->
+    <script src="{{ url('admin/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{ url('admin/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ url('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ url('admin/js/dataTables.select.min.js') }}"></script>
 
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="{{ url('admin/js/off-canvas.js') }}"></script>
-<script src="{{ url('admin/js/hoverable-collapse.js') }}"></script>
-<script src="{{ url('admin/js/template.js') }}"></script>
-<script src="{{ url('admin/js/settings.js') }}"></script>
-<script src="{{ url('admin/js/todolist.js') }}"></script>
-<!-- endinject -->
-<!-- Custom js for this page-->
-<script src="{{ url('admin/js/dashboard.js') }}"></script>
-<script src="{{ url('admin/js/Chart.roundedBarCharts.js') }}"></script>
-<!-- End custom js for this page-->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ url('admin/js/off-canvas.js') }}"></script>
+    <script src="{{ url('admin/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ url('admin/js/template.js') }}"></script>
+    <script src="{{ url('admin/js/settings.js') }}"></script>
+    <script src="{{ url('admin/js/todolist.js') }}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{ url('admin/js/dashboard.js') }}"></script>
+    <script src="{{ url('admin/js/Chart.roundedBarCharts.js') }}"></script>
+    <!-- End custom js for this page-->
 </body>
 
 </html>
-
